ec0c1251baebb4f209b7b29e37a4409fb3482e0b
richardscheid <rscheid@live.com>
2020-05-24 19:29:10 -0300

    configure server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+class App {
+  public express: express.Application;
+
+  public constructor () {
+    this.express = express();
+
+    this.middlewares();
+    this.database();
+    this.routes();
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private database (): void {
+    mongoose.connect('',
+      { useNewUrlParser: true });
+  }
+
+  private routes (): void {
+    this.express.use('/');
+  }
+}
+
+export default new App().express;
