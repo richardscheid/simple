bd558c4fe579e12971b71d1d5fb1774a0bd7a158
richardscheid <rscheid@live.com>
2020-05-28 16:24:26 -0300

    feat: create transaction routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './controllers/UserController';
+import TransactionController from './controllers/TransactionController';
 
 const routes = Router();
 
 routes.get('/users', UserController.all);
 routes.post('/users', UserController.create);
+
+routes.get('/transactions', TransactionController.all);
+routes.post('/transactions', TransactionController.create);
 
 export default routes;
