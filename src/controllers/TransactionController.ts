ccdbc063ac02f0fc77c4d91a54d718f044b71d95
richardscheid <rscheid@live.com>
2020-05-28 15:09:01 -0300

    feat: create transaction endpoint

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -3,9 +3,15 @@ import Transaction from '../models/Transaction';
 
 class TransactionController {
   public async all (req:Request, res:Response): Promise<Response> {
-    const user = Transaction.find();
+    const transaction = await Transaction.find();
 
-    return res.json(user);
+    return res.json(transaction);
+  }
+
+  public async create (req:Request, res:Response): Promise<Response> {
+    const transaction = await Transaction.create(req.body);
+
+    return res.json(transaction);
   }
 }
 
