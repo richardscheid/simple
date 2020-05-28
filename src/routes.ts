66247c482c22af3ec7b0c6d90adafd3525e51728
richardscheid <rscheid@live.com>
2020-05-27 21:43:26 -0300

    fix: change path

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import UserController from './controllers/UserController';
 
 const routes = Router();
 
-routes.get('users/', UserController.all);
-routes.post('users/', UserController.create);
+routes.get('/users', UserController.all);
+routes.post('/users', UserController.create);
 
 export default routes;
