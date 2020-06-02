5e750847770b7431e861a82cdbbf6572d4bf927f
richardscheid <rscheid@live.com>
2020-06-01 21:52:29 -0300

    feat: create transaction function

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 import Transaction from '../models/Transaction';
 
 class TransactionController {
@@ -9,7 +10,18 @@ class TransactionController {
   }
 
   public async create (req:Request, res:Response): Promise<Response> {
-    const transaction = await Transaction.create(req.body);
+    const { amount, place, status } = req.body;
+    const { userid } = req.headers;
+
+    const user = await User.findById(userid);
+    if (!user) return res.status(400).json({ error: 'User does not exists!' });
+
+    const transaction = await Transaction.create({
+      place,
+      amount,
+      status,
+      user: user._id
+    });
 
     return res.json(transaction);
   }
