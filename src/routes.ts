36fd4fe25a5f1912514f83ec5fe3a6e740600a61
richardscheid <rscheid@live.com>
2020-06-01 22:01:46 -0300

    feat: create category routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from './controllers/UserController';
+import CategoryController from './controllers/CategoryController';
 import TransactionController from './controllers/TransactionController';
 
 const routes = Router();
@@ -10,5 +11,8 @@ routes.post('/users', UserController.create);
 
 routes.get('/transactions', TransactionController.all);
 routes.post('/transactions', TransactionController.create);
+
+routes.get('/categories', CategoryController.all);
+routes.post('/categories', CategoryController.create);
 
 export default routes;
