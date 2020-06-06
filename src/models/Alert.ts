88e6da39b9d25026aa332f94b56961fffb811a96
richardscheid <rscheid@live.com>
2020-06-06 18:23:06 -0300

    feat: add alert target

--- src/models/Alert.ts
+++ src/models/Alert.ts
@@ -3,6 +3,7 @@ import { IAlert } from '../interfaces/IAlert';
 
 const AlertSchema = new Schema({
   name: { type: String, required: true },
+  target: { type: Number, required: true },
   condition: { type: Number, required: true }
 }, {
   timestamps: true
