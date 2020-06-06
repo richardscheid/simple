b2bdac7289f6c7d42685a0174fa9150bb8b33dc3
richardscheid <rscheid@live.com>
2020-06-06 18:31:55 -0300

    feat: add base alert validation

--- src/services/AlertService.ts
+++ src/services/AlertService.ts
@@ -1,9 +1,30 @@
 import Alert from '../models/Alert';
+import { IAlert, Condition } from '../interfaces/IAlert';
 import { ITransaction } from '../interfaces/ITransaction';
 
 class AlertService {
   public process (transaction: ITransaction): void {
-    console.log(transaction);
+    const alerts:IAlert[] = Alert.find().lean();
+
+    const amount = transaction.amount;
+
+    for (const alert of alerts) {
+      const cond = alert.condition;
+
+      if (cond === Condition.greaterThan) {
+        if (amount > alert.target) {
+
+        }
+      } else if (cond === Condition.equalsTo) {
+        if (amount === alert.target) {
+
+        }
+      } else if (cond === Condition.lessThan) {
+        if (amount < alert.target) {
+
+        }
+      }
+    }
   }
 }
 
