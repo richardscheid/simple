621d4b54744ff27ee47714b1063e7346b797b90c
richardscheid <rscheid@live.com>
2020-06-07 10:43:49 -0300

    feat: add new properties

--- src/interfaces/IAlert.ts
+++ src/interfaces/IAlert.ts
@@ -1,12 +1,13 @@
 import { Document } from 'mongoose';
 
 export interface IAlert extends Document {
+  _id: number,
   name: string,
   target: number,
   condition: number
 }
 
-export enum Condition {
+export enum Conditions {
   greaterThan,
   equalsTo,
   lessThan
