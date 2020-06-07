0a618f4e92c6d5c67f02ad5b396ec77be2202362
richardscheid <rscheid@live.com>
2020-06-07 10:45:02 -0300

    refact

--- src/services/AlertService.ts
+++ src/services/AlertService.ts
@@ -1,25 +1,25 @@
 import Alert from '../models/Alert';
-import { IAlert, Condition } from '../interfaces/IAlert';
+import { IAlert, Conditions } from '../interfaces/IAlert';
 import { ITransaction } from '../interfaces/ITransaction';
 
 class AlertService {
   public process (transaction: ITransaction): void {
-    const alerts:IAlert[] = Alert.find().lean();
+    const alerts = Alert.find().lean();
 
     const amount = transaction.amount;
 
     for (const alert of alerts) {
-      const cond = alert.condition;
+      const cond = alert.conditions;
 
-      if (cond === Condition.greaterThan) {
+      if (cond === Conditions.greaterThan) {
         if (amount > alert.target) {
 
         }
-      } else if (cond === Condition.equalsTo) {
+      } else if (cond === Conditions.equalsTo) {
         if (amount === alert.target) {
 
         }
-      } else if (cond === Condition.lessThan) {
+      } else if (cond === Conditions.lessThan) {
         if (amount < alert.target) {
 
         }
