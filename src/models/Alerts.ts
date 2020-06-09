c8c7685c9cb7a4f8eb654337754b5bbdf559dcb9
richardscheid <rscheid@live.com>
2020-06-09 19:23:39 -0300

    feat: create alerts model

---
+++ src/models/Alerts.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import { IAlerts } from '../interfaces/IAlerts';
+
+const AlertsSchema = new Schema({
+  name: { type: String, required: true },
+  target: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  status: { type: Number, required: true },
+  condition: { type: Number, required: true },
+  alert: {
+    type: Schema.Types.ObjectId,
+    ref: 'Alert',
+    required: true
+  },
+  transaction: {
+    type: Schema.Types.ObjectId,
+    ref: 'Transaction',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+export default model<IAlerts>('Alerts', AlertsSchema);
