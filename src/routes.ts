37328a5ccfadd3269398e47b08f467380d4854ce
richardscheid <rscheid@live.com>
2020-06-11 20:37:13 -0300

    feat: add alerts route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './controllers/UserController';
 import AlertController from './controllers/AlertController';
+import AlertsController from './controllers/AlertsController';
 import CategoryController from './controllers/CategoryController';
 import TransactionController from './controllers/TransactionController';
 
@@ -12,6 +13,8 @@ routes.post('/users', UserController.create);
 
 routes.get('/alert', AlertController.all);
 routes.post('/alert', AlertController.create);
+
+routes.get('/alerts', AlertsController.all);
 
 routes.get('/categories', CategoryController.all);
 routes.post('/categories', CategoryController.create);
