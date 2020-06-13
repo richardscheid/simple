1759a70f26e073618cbb49e042bd1f447a745765
richardscheid <rscheid@live.com>
2020-06-13 12:19:36 -0300

    feat: update transactions on alerts

--- src/services/AlertService.ts
+++ src/services/AlertService.ts
@@ -3,18 +3,33 @@ import Alerts from '../models/Alerts';
 import { IAlerts } from '../interfaces/IAlerts';
 import { Conditions, IAlert } from '../interfaces/IAlert';
 import { ITransaction, Status } from '../interfaces/ITransaction';
+import Transaction from '../models/Transaction';
 
 class AlertService {
   async process (transaction: ITransaction): Promise<void> {
     const alerts = await Alert.find().populate('category');
 
     const amount = transaction.amount;
+    const alertsId : number[] = [];
 
     for (const alert of alerts) {
       const onAlert: boolean = this.verify(amount, alert.target, alert.condition);
 
-      if (onAlert) this.create(alert, transaction);
+      if (onAlert) {
+        const result = this.create(alert, transaction);
+
+        if (result) alertsId.push((await result)._id);
+      }
     }
+
+    if (alertsId) this.update(alertsId, transaction);
+  }
+
+  async update (alertsId:number[], transaction:ITransaction):Promise<void> {
+    const filter = <ITransaction>{ _id: transaction._id };
+    const update = <ITransaction>{ status: Status.Onalert };
+
+    await Transaction.findByIdAndUpdate(filter, update);
   }
 
   async create (alert: IAlert, transaction: ITransaction):Promise<IAlerts> {
