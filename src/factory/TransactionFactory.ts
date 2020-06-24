eb351e2044a94612bfba30789e643a732264823d
richardscheid <rscheid@live.com>
2020-06-24 16:50:34 -0300

    feat: add build transaction feature

--- src/factory/TransactionFactory.ts
+++ src/factory/TransactionFactory.ts
@@ -1,6 +1,20 @@
+import Transaction from '../models/Transaction';
+import { Status, ITransaction } from '../interfaces/ITransaction';
 
 class TransactionFactory {
+  public build (amount, place, order, company, items, user, category): ITransaction {
+    const trn = new Transaction();
+    trn.place = place;
+    trn.order = order;
+    trn.items = items;
+    trn.amount = amount;
+    trn.company = company;
+    trn.user = user._id;
+    trn.category = category._id;
+    trn.status = Status.Unverified;
 
+    return trn;
+  }
 }
 
 export default new TransactionFactory();
