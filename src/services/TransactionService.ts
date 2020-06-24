4ce089ea16e6c070a2767cb2da3af17a05feec22
richardscheid <rscheid@live.com>
2020-06-24 16:47:58 -0300

    feat: create transaction service

---
+++ src/services/TransactionService.ts
@@ -0,0 +1,18 @@
+import Transaction from '../models/Transaction';
+import { ITransaction } from '../interfaces/ITransaction';
+
+class TransactionService {
+  async findAll () {
+    return await Transaction.find();
+  }
+
+  async findOne (place:string) {
+    return await Transaction.findOne(<ITransaction>{ place });
+  }
+
+  async create (transaction:ITransaction) {
+    return await Transaction.create(transaction);
+  }
+}
+
+export default new TransactionService();
