a08f476e46eaea19c32fb88d94cc98e597d186a4
richardscheid <rscheid@live.com>
2020-06-27 13:20:37 -0300

    feat: add login/logout routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,31 @@
 import { Router } from 'express';
 
+import AuthController from './controllers/auth.controller';
 import UserController from './controllers/user.controller';
 import AlertController from './controllers/alert.controller';
 import AlertsController from './controllers/alerts.controller';
 import CategoryController from './controllers/category.controller';
 import TransactionController from './controllers/transaction.controller';
 
+import { isAuthenticated } from './config/passport';
+
 const routes = Router();
 
-routes.get('/users', UserController.all);
-routes.post('/users', UserController.create);
+routes.get('/login', AuthController.login);
+routes.post('/logout', AuthController.logout);
 
-routes.get('/alert', AlertController.all);
-routes.post('/alert', AlertController.create);
+routes.get('/users', isAuthenticated, UserController.all);
+routes.post('/users', isAuthenticated, UserController.create);
 
-routes.get('/alerts', AlertsController.all);
+routes.get('/alert', isAuthenticated, AlertController.all);
+routes.post('/alert', isAuthenticated, AlertController.create);
 
-routes.get('/categories', CategoryController.all);
-routes.post('/categories', CategoryController.create);
+routes.get('/alerts', isAuthenticated, AlertsController.all);
 
-routes.get('/transactions', TransactionController.all);
-routes.post('/transactions', TransactionController.create);
+routes.get('/categories', isAuthenticated, CategoryController.all);
+routes.post('/categories', isAuthenticated, CategoryController.create);
+
+routes.get('/transactions', isAuthenticated, TransactionController.all);
+routes.post('/transactions', isAuthenticated, TransactionController.create);
 
 export default routes;
