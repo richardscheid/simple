26e5d86fca9c77a85502aa2209a28b23b94c4f2e
richardscheid <rscheid@live.com>
2020-06-27 14:52:49 -0300

    feat: create category service

---
+++ src/services/category.service.ts
@@ -0,0 +1,22 @@
+import Category from '../models/Category';
+import { ICategory } from '../interfaces/category.interface';
+
+class CategoryService {
+  async findAll ():Promise<ICategory[]> {
+    return await Category.find();
+  }
+
+  async findById (id:string):Promise<ICategory | null> {
+    return await Category.findById(id);
+  }
+
+  async findOne (name:string): Promise<ICategory | null> {
+    return await Category.findOne(<ICategory>{ name });
+  }
+
+  async create (category:ICategory): Promise<ICategory> {
+    return await Category.create(category);
+  }
+}
+
+export default new CategoryService();
