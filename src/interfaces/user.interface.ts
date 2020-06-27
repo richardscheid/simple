dbafdbd7ef58d72272e54f2fae48a888b9c786d9
richardscheid <rscheid@live.com>
2020-06-27 13:21:46 -0300

    feat: add passport authenticate

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -3,5 +3,14 @@ import { Document } from 'mongoose';
 export interface IUser extends Document {
   name: string,
   email: string,
-  password: string
+  password: string,
+  tokens: AuthToken[],
+  comparePassword: comparePasswordFunction
 }
+
+export interface AuthToken {
+  kind:string;
+  accessToken: string;
+}
+
+export type comparePasswordFunction = (candidatePassword: string, callback: (err: any, isMatch: any) => unknown) => void;
