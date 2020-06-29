e8a3e58cc7279fff3401e4ab25b1e98986da1629
richardscheid <rscheid@live.com>
2020-06-28 21:30:35 -0300

    refact: remove function type

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -5,12 +5,10 @@ export interface IUser extends Document {
   email: string,
   password: string,
   tokens: AuthToken[],
-  comparePassword: comparePasswordFunction
+  validatePassword (candidatePassword:string): boolean
 }
 
 export interface AuthToken {
-  kind:string;
+  kind: string;
   accessToken: string;
 }
-
-export type comparePasswordFunction = (candidatePassword: string, callback: (err: any, isMatch: any) => unknown) => void;
