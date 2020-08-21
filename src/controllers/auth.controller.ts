affbe262751217ba7bbf7691dc4527c7897bfeda
richardscheid <rscheid@live.com>
2020-08-21 19:38:09 -0300

    chore: update auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,24 +1,26 @@
+import { JWT_SECRET } from '@utils/secrets';
 import { IUser } from '@interfaces/user.interface';
 import { Request, Response, NextFunction } from 'express';
 
+import jwt from 'jsonwebtoken';
 import passport from 'passport';
 
 class AuthController {
   async login (req: Request, res:Response, next: NextFunction) {
     passport.authenticate('local', { failureFlash: true }, (err: Error, user: IUser) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
 
-      if (!user) {
-        return res.status(401).end();
-      }
+      if (!user) return res.status(401).end();
 
       req.logIn(user, (err) => {
-        if (err) { return next(err); }
-        return res.status(200).send({
-          auth: true,
-          token: 123
+        if (err) return next(err);
+
+        const payload = { email: user.email };
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
+        return res.json({
+          token,
+          auth: true
         });
       });
     })(req, res, next);
