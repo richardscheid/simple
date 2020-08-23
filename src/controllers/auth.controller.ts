b56140adb26e70cb34f63fc37af40b94967d3b9b
richardscheid <rscheid@live.com>
2020-08-23 17:02:20 -0300

    chore: create auth middleware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,20 +2,21 @@ import { JWT_SECRET } from '@utils/secrets';
 import { IUser } from '@interfaces/user.interface';
 import { Request, Response, NextFunction } from 'express';
 
+import '../config/passport';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
 
 class AuthController {
   async login (req: Request, res:Response, next: NextFunction) {
-    passport.authenticate('local', { failureFlash: true }, (err: Error, user: IUser) => {
+    passport.authenticate('login', { failureFlash: true }, (err: Error, user: IUser) => {
       if (err) return next(err);
 
       if (!user) return res.status(401).end();
 
-      req.logIn(user, (err) => {
+      req.logIn(user, { session: false }, (err) => {
         if (err) return next(err);
 
-        const payload = { email: user.email };
+        const payload = { sub: user.email };
         const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 
         return res.json({
@@ -24,6 +25,10 @@ class AuthController {
         });
       });
     })(req, res, next);
+  }
+
+  authenticate (req: Request, res: Response, next: NextFunction) {
+    passport.authenticate('jwt', { session: false })(req, res, next);
   }
 
   async logout (req: Request, res: Response) {
