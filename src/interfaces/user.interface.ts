2bcc02282b0d43968bf282ef5807115c3c606a51
richardscheid <rscheid@live.com>
2020-08-24 17:14:23 -0300

    chore: remove unnecessary code

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -4,11 +4,5 @@ export interface IUser extends Document {
   name: string,
   email: string,
   password: string,
-  tokens: AuthToken[],
   validatePassword (candidatePassword:string): boolean
-}
-
-export interface AuthToken {
-  kind: string;
-  accessToken: string;
 }
