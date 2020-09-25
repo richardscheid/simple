97e983693ac936e6a185489e950c4daed14b7c1c
richardscheid <rscheid@live.com>
2020-09-25 11:17:44 -0300

    chore: authorizing token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -9,9 +9,9 @@ import passport from 'passport';
 class AuthController {
   async login (req: Request, res:Response, next: NextFunction) {
     passport.authenticate('login', { failureFlash: true }, (err: Error, user: IUser) => {
-      if (err) return next(err);
+      if (err) return res.status(400).json({ auth: false, message: 'Authentication failed! Please check the email and password.' });
 
-      if (!user) return res.status(401).end();
+      if (!user) return res.status(403).json({ auth: false, message: 'Incorrect email or password.' });
 
       req.logIn(user, { session: false }, (err) => {
         if (err) return next(err);
@@ -29,6 +29,22 @@ class AuthController {
 
   authenticate (req: Request, res: Response, next: NextFunction) {
     passport.authenticate('jwt', { session: false })(req, res, next);
+  }
+
+  authorize (req: Request, res:Response, next: NextFunction) {
+    let token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+    if (!token.startsWith('Bearer ')) return res.status(401).json({ auth: false, message: 'Token format is not valid.' });
+
+    token = token.slice(7, token.length).trimLeft();
+
+    jwt.verify(token, JWT_SECRET, (err) => {
+      if (err) return res.status(403).json({ auth: false, message: 'Token is not valid.' });
+
+      return res.json({ auth: true });
+    });
   }
 
   async logout (req: Request, res: Response) {
