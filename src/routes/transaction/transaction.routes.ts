f1a15aa16d53319579ef6b7db1dcd1179d560354
richardscheid <rscheid@live.com>
2020-10-01 17:42:41 -0300

    chore: validate transaction data

--- src/routes/transaction/transaction.routes.ts
+++ src/routes/transaction/transaction.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import AuthController from '@controllers/auth/auth.controller';
 import TransactionController from '@controllers/transaction/transaction.controller';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 class TransactionRoutes {
   public routes = Router();
@@ -12,7 +13,21 @@ class TransactionRoutes {
 
   router ():void {
     this.routes.get('/', AuthController.authenticate, TransactionController.all);
-    this.routes.post('/', AuthController.authenticate, TransactionController.create);
+
+    this.routes.post('/', celebrate({
+      [Segments.HEADERS]: Joi.object({
+        user_id: Joi.string().required(),
+        category_name: Joi.string().required()
+      }).unknown(),
+      [Segments.BODY]: Joi.object().keys({
+        place: Joi.string().required(),
+        order: Joi.number().integer().required(),
+        amount: Joi.number().required(),
+        company: Joi.string().required(),
+        items: Joi.array().required()
+      })
+    })
+    , AuthController.authenticate, TransactionController.create);
   }
 }
 
