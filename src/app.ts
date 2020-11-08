480cea1ac0d5a95de924d550f89987f80ed941c7
richardscheid <rscheid@live.com>
2020-11-08 17:43:04 -0300

    chore: setup i18n

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,57 @@
-import cors from 'cors';
-import path from 'path';
-import helmet from 'helmet';
-import express from 'express';
-import mongoose from 'mongoose';
-import passport from 'passport';
-import flash from 'express-flash';
-import compression from 'compression';
-import limiter from 'express-rate-limit';
-import i18next from 'i18next';
-import Backend from 'i18next-fs-backend';
-import i18nextMiddleware from 'i18next-express-middleware';
+import cors from 'cors'
+import path from 'path'
+import helmet from 'helmet'
+import express from 'express'
+import i18next from 'i18next'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import flash from 'express-flash'
+import compression from 'compression'
+import limiter from 'express-rate-limit'
+import Backend from 'i18next-fs-backend'
+import i18n from 'i18next-http-middleware'
+import { errors } from 'celebrate'
 
-import { errors } from 'celebrate';
-
-import routes from './routes/routes';
-import { MONGODB_URI } from './utils/secrets';
+import { MONGODB_URI } from './utils/secrets'
+import routes from './routes/routes'
 
 class App {
   public express: express.Application;
 
   public constructor () {
-    this.express = express();
+    this.express = express()
 
-    this.middlewares();
-    this.database();
-    this.routes();
+    this.middlewares()
+    this.database()
+    this.routes()
   }
 
   private middlewares (): void {
-    this.express.use(flash());
-    this.express.use(helmet());
-    this.express.use(errors());
-    this.express.use(limiter());
-    this.express.use(compression());
-    this.express.use(express.json());
-    this.express.use(express.urlencoded({ extended: false }));
-    this.express.use(passport.initialize());
-    this.i18n();
-    this.cors();
+    this.express.use(flash())
+    this.express.use(helmet())
+    this.express.use(errors())
+    this.express.use(limiter())
+    this.express.use(compression())
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(passport.initialize())
+    this.i18n()
+    this.cors()
   }
 
   private i18n (): void {
     i18next
       .use(Backend)
-      .use(i18nextMiddleware.LanguageDetector)
+      .use(i18n.LanguageDetector)
       .init({
         backend: {
-          loadPath: path.join(__dirname, '/../resources/locales/{{lng}}/{{ns}}.json')
+          loadPath: path.join(__dirname, '/../resources/locales/{{lng}}/translation.json')
         },
-        fallbackLng: 'en',
+        fallbackLng: 'pt',
         preload: ['en', 'es', 'pt']
-      });
+      })
 
-    this.express.use(i18nextMiddleware.handle(i18next));
+    this.express.use(i18n.handle(i18next))
   }
 
   private cors (): void {
@@ -62,7 +61,7 @@ class App {
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true
       })
-    );
+    )
   }
 
   private database (): void {
@@ -70,12 +69,12 @@ class App {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
-    });
+    })
   }
 
   private routes (): void {
-    this.express.use(routes);
+    this.express.use(routes)
   }
 }
 
-export default new App().express;
+export default new App().express
