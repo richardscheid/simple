3f0ac814db9b18944990f766b0925e85bf407015
richardscheid <rscheid@live.com>
2020-11-09 19:41:07 -0300

    chore: create user not found error

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,26 +1,29 @@
-import { Request, Response } from 'express';
-import UserService from '@services/user/user.service';
+import UserService from '@services/user/user.service'
+import { Request, Response } from 'express'
+import i18next from 'i18next'
 
 class UserController {
-  public async all (req: Request, res:Response):Promise<Response> {
-    const users = await UserService.findAll();
+  public async all (req: Request, res: Response): Promise<Response> {
+    const users = await UserService.findAll()
 
-    return res.json(users);
+    return res.json(users)
   }
 
-  public async findById (req: Request, res:Response):Promise<Response> {
-    const { id } = req.params;
+  public async findById (req: Request, res:Response): Promise<Response> {
+    const { id } = req.params
 
-    const users = await UserService.findById(id as string);
+    const users = await UserService.findById(id as string)
 
-    return res.json(users);
+    if (!users) return res.status(400).send(i18next.t('error.notfound.user'))
+
+    return res.json(users)
   }
 
-  public async create (req:Request, res:Response): Promise<Response> {
-    const user = await UserService.create(req.body);
+  public async create (req: Request, res: Response): Promise<Response> {
+    const user = await UserService.create(req.body)
 
-    return res.json(user);
+    return res.json(user)
   }
 }
 
-export default new UserController();
+export default new UserController()
