b34478a4d4e18031ac856bf0ad204be4726ee9b6
richardscheid <rscheid@live.com>
2020-11-21 17:51:24 -0300

    chore: update http status code

--- src/resources/codes/codes.ts
+++
@@ -1,4 +0,0 @@
-
-export const CODE_OK = 200
-export const CODE_NOT_FOUND = 404
-export const CODE_UNAUTHORIZED = 400
---
+++ src/resources/codes/http.statuscode.ts
@@ -0,0 +1,15 @@
+
+export enum HttpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER = 500
+}
+
+export namespace HttpStatus {
+  export function type (code: HttpStatusCode): string {
+    return HttpStatusCode[code]
+  }
+}
