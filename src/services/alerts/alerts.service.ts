46bc16df3a6fffaf9ad1cfa614bbff4210662a71
richardscheid <rscheid@live.com>
2020-11-22 14:04:11 -0300

    chore: improving code readability

--- src/services/alerts/alerts.service.ts
+++ src/services/alerts/alerts.service.ts
@@ -1,40 +1,41 @@
-import { IAlerts } from '@interfaces/alerts/alerts.interface';
-import { AlertsBuilder } from '@builders/alerts/alerts.builder';
-import { Conditions, IAlert } from '@interfaces/alert/alert.interface';
-import { ITransaction, Status } from '@interfaces/transaction/transaction.interface';
+import { ITransaction, Status } from '@interfaces/transaction/transaction.interface'
+import { Conditions, IAlert } from '@interfaces/alert/alert.interface'
+import { AlertsBuilder } from '@builders/alerts/alerts.builder'
+import { IAlerts } from '@interfaces/alerts/alerts.interface'
 
-import Alert from '@models/alert/alert';
-import Alerts from '@models/alerts/Alerts';
-import Transaction from '@models/transaction/transaction';
+import Transaction from '@models/transaction/transaction'
+import Alerts from '@models/alerts/Alerts'
+import Alert from '@models/alert/alert'
 
 class AlertService {
-  async process (transaction: ITransaction): Promise<void> {
-    const alerts = await Alert.find().populate('category');
 
-    const amount = transaction.amount;
-    const alertsId : string[] = [];
+  async process (transaction: ITransaction): Promise<void> {
+    const alerts = await Alert.find().populate('category')
+
+    const amount = transaction.amount
+    const alertsId : string[] = []
 
     for (const alert of alerts) {
-      const onAlert: boolean = this.verify(amount, alert.target, alert.condition);
+      const onAlert: boolean = this.verify(amount, alert.target, alert.condition)
 
       if (onAlert) {
-        const result = this.create(alert, transaction);
+        const result = this.create(alert, transaction)
 
-        if (result) alertsId.push((await result)._id);
+        if (result) alertsId.push((await result)._id)
       }
     }
 
-    if (alertsId) this.update(transaction);
+    if (alertsId) this.update(transaction)
   }
 
-  async update (transaction:ITransaction):Promise<void> {
-    const filter = <ITransaction>{ _id: transaction._id };
-    const update = <ITransaction>{ status: Status.Onalert };
+  async update (transaction: ITransaction): Promise<void> {
+    const filter = <ITransaction>{ _id: transaction._id }
+    const update = <ITransaction>{ status: Status.Onalert }
 
-    await Transaction.findByIdAndUpdate(filter, update);
+    await Transaction.findByIdAndUpdate(filter, update)
   }
 
-  async create (alert: IAlert, transaction: ITransaction):Promise<IAlerts> {
+  async create (alert: IAlert, transaction: ITransaction): Promise<IAlerts> {
     return await Alerts.create(new AlertsBuilder()
       .name(alert.name)
       .target(alert.target)
@@ -44,29 +45,29 @@ class AlertService {
       .alert(alert)
       .transaction(transaction)
       .build()
-    );
+    )
   }
 
   public verify (amount: number, target: number, condition: number): boolean {
     switch (condition) {
       case Conditions.GreaterThan:
-        return amount > target;
+        return amount > target
 
       case Conditions.GreaterThanEquals:
-        return amount >= target;
+        return amount >= target
 
       case Conditions.EqualsTo:
-        return amount === target;
+        return amount === target
 
       case Conditions.LessThanEquals:
-        return amount <= target;
+        return amount <= target
 
       case Conditions.LessThan:
-        return amount < target;
+        return amount < target
     }
 
-    return false;
+    return false
   }
 }
 
-export default new AlertService();
+export default new AlertService()
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -3,19 +3,19 @@ import { IUser } from '@interfaces/user/user.interface'
 
 class UserService {
 
-  async findAll ():Promise<IUser[]> {
+  async findAll (): Promise<IUser[]> {
     return await User.find()
   }
 
-  async findById (_id: string): Promise<IUser | null> {
+  async findById (_id: string) {
     return await User.findById(<IUser>{ _id })
   }
 
-  async findOne (email:string, callback?): Promise<IUser | null> {
+  async findOne (email: string, callback?) {
     return await User.findOne(<IUser>{ email: email.toLowerCase() }, callback)
   }
 
-  async create (user:IUser): Promise<IUser> {
+  async create (user: IUser): Promise<IUser> {
     return await User.create(user)
   }
 }
