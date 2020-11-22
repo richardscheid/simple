16788cdba8abd8224c839dde16e853094e4e3a6a
richardscheid <rscheid@live.com>
2020-11-22 15:18:36 -0300

    chore: validate for required fields on request

--- src/routes/alert/alert.routes.ts
+++ src/routes/alert/alert.routes.ts
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import { celebrate, Segments, Joi } from 'celebrate'
 import { Router } from 'express'
 
 import AlertController from '@controllers/alert/alert.controller'
@@ -14,7 +15,20 @@ class AlertRoutes {
 
   router (): void {
     this.routes.get('/', AuthController.authenticate, asyncHandler(AlertController.findAll))
-    this.routes.post('/', AuthController.authenticate, asyncHandler(AlertController.create))
+
+    this.routes.post('/',
+      celebrate({
+        [Segments.HEADERS]: Joi.object({
+          category_id: Joi.string().required()
+        }).unknown(),
+        [Segments.BODY]: Joi.object().keys({
+          name: Joi.string().required(),
+          target: Joi.number().positive().required(),
+          condition: Joi.number().integer().min(0).max(4).required()
+        })
+      }),
+      AuthController.authenticate,
+      asyncHandler(AlertController.create))
   }
 }
 
