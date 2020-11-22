66a308384d0c63b00f91cc2ba82d706c03bd5aea
richardscheid <rscheid@live.com>
2020-11-22 11:45:02 -0300

    chore: handle async routes

--- src/routes/category/category.routes.ts
+++ src/routes/category/category.routes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express'
 import asyncHandler from 'express-async-handler'
+import { celebrate, Segments, Joi } from 'celebrate'
+import { Router } from 'express'
 
 import CategoryController from '@controllers/category/category.controller'
 import AuthController from '@controllers/auth/auth.controller'
 
 class CategoryRoutes {
+
   public routes = Router();
 
   constructor () {
@@ -12,7 +14,14 @@ class CategoryRoutes {
   }
 
   router ():void {
-    this.routes.get('/', AuthController.authenticate, asyncHandler(CategoryController.all))
+    this.routes.get('/', AuthController.authenticate, asyncHandler(CategoryController.findAll))
+
+    this.routes.get('/:id/details', celebrate({
+      [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required()
+      })
+    }), AuthController.authenticate, asyncHandler(CategoryController.findById))
+
     this.routes.post('/', AuthController.authenticate, asyncHandler(CategoryController.create))
   }
 }
