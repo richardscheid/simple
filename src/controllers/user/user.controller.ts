eaa2e765a4ad9a82355d8d70a62024f8839d705d
richardscheid <rscheid@live.com>
2020-11-26 19:01:59 -0300

    chore: use of user builder

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,9 +1,12 @@
 import Exception from '@resources/exceptions/exception'
 import UserService from '@services/user/user.service'
+import BankService from '@services/bank/bank.service'
 import i18next from 'i18next'
 
 import { HttpStatusCode } from '@resources/codes/http.statuscode'
 import { Request, Response } from 'express'
+import { IBank } from '../../interfaces/bank/bank.interface'
+import { UserBuilder } from '../../builders/user/user.builder'
 
 class UserController {
 
@@ -30,7 +33,23 @@ class UserController {
 
     if (exists) throw new Exception(HttpStatusCode.ALREADY_EXISTS, i18next.t('error.user.alreadyexists'))
 
-    const user = await UserService.create(req.body)
+    const { bank_id } = req.headers
+
+    const bank = await BankService.findById(bank_id as string)
+
+    if (!bank) throw new Exception(HttpStatusCode.NOT_FOUND, i18next.t('error.bank.notfound'))
+
+    const { username, password, document, account } = req.body
+
+    const user = await UserService.create(
+      new UserBuilder()
+        .email(email)
+        .username(username)
+        .password(password)
+        .document(document)
+        .account(account, bank)
+        .build()
+    )
 
     return res.json(user)
   }
