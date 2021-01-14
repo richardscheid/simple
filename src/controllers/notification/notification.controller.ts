8907d0c127436700239b1967b05df8ab303f376b
richardscheid <rscheid@live.com>
2021-01-14 17:50:42 -0300

    feat: create search notification by id controller

--- src/controllers/notification/notification.controller.ts
+++ src/controllers/notification/notification.controller.ts
@@ -1,4 +1,5 @@
 import NotificationGateway from '@gateways/notification/notification.gateway'
+import NotificationService from '@services/notification/notification.service'
 import { Request, Response } from 'express'
 import Container, { Service } from 'typedi'
 
@@ -7,6 +8,14 @@ class NotificationController {
 
   async findAll (req: Request, res: Response): Promise<Response> {
     const notifications = await NotificationGateway.findAll()
+
+    return res.json(notifications)
+  }
+
+  async findById (req: Request, res: Response): Promise<Response> {
+    const { id } = req.params
+
+    const notifications = await NotificationService.getNotificationById(id)
 
     return res.json(notifications)
   }
