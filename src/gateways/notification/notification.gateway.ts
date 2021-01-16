cb12a1f7717a5e071ca317095c83b86f0dd5ee04
richardscheid <rscheid@live.com>
2021-01-16 09:37:16 -0300

    chore: change notification gateway

--- src/gateways/notification/notification.gateway.ts
+++ src/gateways/notification/notification.gateway.ts
@@ -2,6 +2,7 @@ import Container, { Service } from 'typedi'
 import { INotification } from '@interfaces/notification/notification.interface'
 import { INotificationGateway } from './notification.gateway.interface'
 import Notification from '@models/notification/notification'
+import { IUser } from '@interfaces/user/user.interface'
 
 @Service()
 class NotificationGateway implements INotificationGateway {
@@ -14,8 +15,9 @@ class NotificationGateway implements INotificationGateway {
     return await Notification.create(notification)
   }
 
-  async findById (_id: string): Promise<INotification | null> {
-    return await Notification.findById(_id).populate('transaction')
+  async findById (userid: string): Promise<INotification[]> {
+    const filter = <IUser>{ _id: userid }
+    return await Notification.find({ user: filter }).populate('transaction')
   }
 }
 
