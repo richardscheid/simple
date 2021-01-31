3e2925c338d3102b9ccae7357e35fe6c8f67b80f
richardscheid <rscheid@live.com>
2021-01-31 15:24:08 -0300

    refact: fix imports at server

--- src/application/server/server.ts
+++ src/application/server/server.ts
@@ -1,5 +1,5 @@
-import app from './app'
-import { logger } from '@utils/logger'
+import app from '../app'
+import { logger } from '@resources/utils/logger'
 
 app.listen(process.env.PORT, () => {
   logger.info(`> Server ready on port: ${process.env.PORT}`)
